2f03b73d78bbbd50197823dbcb8c53647d9be301
darwinum <dun34@hotmail.com>
2019-08-15 15:58:34 -0500

    se crea el metodo  guardar imagen

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+/* import = as firebase from 'firebase'; */
+
 
 @Injectable()
 export class CargaImagenesService {
+  private CARPETA_IMAGENES = 'img';
+  constructor(private db: AngularFirestore) {
 
-  constructor(db: AngularFirestore) {
+  }
 
+  private guardarImagen(imagen: { nombre: string, url: string }) {
+    this.db.collection(`${this.CARPETA_IMAGENES}`).add(imagen);
   }
 }
