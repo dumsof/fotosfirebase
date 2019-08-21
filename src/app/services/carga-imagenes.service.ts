ff2a97f7e732db9c578f06f34716bc8f912747b4
darwinum <dun34@hotmail.com>
2019-08-21 11:16:24 -0500

    metodo carga imagen store firebase base datos

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -3,6 +3,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 /* import = as firebase from 'firebase'; */
 import { FileItem } from '../components/models/file-item';
 
+/* necesita paquete firebase, se coloca para quitar el error */
+import * as firebase from 'firebase';
+
 
 @Injectable()
 export class CargaImagenesService {
@@ -12,6 +15,31 @@ export class CargaImagenesService {
   }
 
   cargarImagenesFireBase(imagenes: FileItem[]) {
+    const storageRef = firebase.storage().ref();
+
+    for (const item of imagenes) {
+      if (item.progreso >= 100) {
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask =
+        storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`).put(item.archivo);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        (error) => console.log('Error al subir el archivo ', error),
+        () => {
+          console.log('Imagen cargada correctamente.');
+          item.url = uploadTask.snapshot.downloadURL;
+          item.estaSubiendo = false;
+          /* despues que se guarda en el storage de firebase, se guarda una referencia en la base de datos */
+          this.guardarImagen({
+            nombre: item.nombreArchivo,
+            url: item.url
+          });
+        }
+      );
+    }
+
     console.log(imagenes);
   }
 
